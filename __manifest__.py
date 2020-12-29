5520b279cfe0031922e96f57ea7251cd218d052b
alonewerwolf <andriy.zhigil@gmail.com>
2020-12-29 13:16:18 +0200

    site templates

--- __manifest__.py
+++ __manifest__.py
@@ -22,6 +22,7 @@
     # any module necessary for this one to work correctly
     'depends': ['base',
                 'website',
+                'web',
                 ],
 
     # always loaded
--- controllers/controllers.py
+++ controllers/controllers.py
@@ -3,19 +3,44 @@ from odoo import http
 
 
 class Bands(http.Controller):
-    @http.route('/bands/musicians/', auth='public', website=True)
-    def musicians(self, **kw):
-        Musicians = http.request.env['bands.musicians']
-        return http.request.render('bands.musician_page', {
-            'musicians': Musicians.search([])
-        })
+    @http.route('/uber-uns', auth='public', website=True)
+    def uber_uns(self, **kw):
+        return http.request.render('bands.uber-uns')
 
-    @http.route('/bands/<model("bands.musicians"):musician>/', auth='public',
-                website=True)
-    def musician(self, musician):
-        return http.request.render('bands.biography', {
-            'person': musician
-        })
+    @http.route('/uber-uns/geschichte', auth='public', website=True)
+    def geschichte(self, **kw):
+        return http.request.render('bands.geschichte')
+
+    @http.route('/uber-uns/philosophie', auth='public', website=True)
+    def philosophie(self, **kw):
+        return http.request.render('bands.philosophie')
+
+    @http.route('/uber-uns/offene-stellen', auth='public', website=True)
+    def offene_stellen(self, **kw):
+        return http.request.render('bands.offene-stellen')
+
+    @http.route('/uber-uns/zertifizierung', auth='public', website=True)
+    def zertifizierung(self, **kw):
+        return http.request.render('bands.zertifizierung')
+
+    @http.route('/uber-uns/partner', auth='public', website=True)
+    def partner(self, **kw):
+        return http.request.render('bands.partner')
+
+    @http.route('/uber-uns/messen', auth='public', website=True)
+    def messen(self, **kw):
+        return http.request.render('bands.messen')
+
+    @http.route('/uber-uns/aktuelles', auth='public', website=True)
+    def aktuelles(self, **kw):
+        return http.request.render('bands.aktuelles')
+
+    # @http.route('/bands/<model("bands.musicians"):musician>/', auth='public',
+    #             website=True)
+    # def musician(self, musician):
+    #     return http.request.render('bands.biography', {
+    #         'person': musician
+    #     })
 
 #     @http.route('/bands/bands/objects/', auth='public')
 #     def list(self, **kw):
